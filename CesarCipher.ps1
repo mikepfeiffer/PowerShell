66f04b1b9bf0f5ca3a1237072a345a83f97a59f0
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:04:55 -0700

    Convert to/from a basic ceasar cipher

---
+++ CesarCipher.ps1
@@ -0,0 +1,35 @@
+﻿function ConvertTo-CesarCipher() {
+    param($String)
+
+    begin {
+        $sb = New-Object System.Text.StringBuilder
+    }
+
+    process {
+        $String.ToCharArray() | %{
+            $null = $sb.Append([char](([int][char]$_) +1))            
+        }
+    }
+
+    end {
+        $sb.ToString()
+    }
+}
+
+function ConvertFrom-CesarCipher() {
+    param($String)
+
+    begin {
+        $sb = New-Object System.Text.StringBuilder
+    }
+
+    process {
+        $String.ToCharArray() | %{
+            $null = $sb.Append([char](([int][char]$_) -1))            
+        }
+    }
+
+    end {
+        $sb.ToString()
+    }
+}
