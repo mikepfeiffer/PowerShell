cf4667bf0e0760226fd5280e7c12d3529d2988d5
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:05:28 -0700

    Find duplicate array values

---
+++ Get-Duplicate.ps1
@@ -0,0 +1,20 @@
+﻿function Get-Duplicate {
+    param($array, [switch]$count)
+    begin {
+        $hash = @{}
+    }
+    process {
+        $array | %{ $hash[$_] = $hash[$_] + 1 }
+        if($count) {
+            $hash.GetEnumerator() | ?{$_.value -gt 1} | %{
+                New-Object PSObject -Property @{
+                    Value = $_.key
+                    Count = $_.value
+                }
+            }
+        }
+        else {
+            $hash.GetEnumerator() | ?{$_.value -gt 1} | %{$_.key}
+        }    
+    }
+}
