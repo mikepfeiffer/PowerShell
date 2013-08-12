649d311f745aa8aadee8f234ba903652b33015a3
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:06:54 -0700

    Submit an email via the pickup directory

---
+++ Submit-PickupMessage.ps1
@@ -0,0 +1,16 @@
+﻿function Submit-PickupMessage {
+	Param($To, $From, $Subject, $Message, $Bcc, $TransportServer)
+
+	$string = New-Object System.Text.StringBuilder
+	$string.AppendLine("To:$($To -join ';')") | Out-Null
+	if($Bcc) {$string.AppendLine("Bcc:$($Bcc -join ';')") | Out-Null}
+	$string.AppendLine("From:$From") | Out-Null
+	$string.AppendLine("Subject:$Subject") | Out-Null
+	$string.AppendLine() | Out-Null
+	$string.AppendLine($Message) | Out-Null
+	
+	$path = (Get-TransportServer -identity $TransportServer).PickupDirectoryPath
+	$path = "\\$TransportServer\$($path.ToString().replace(':','$'))"
+	$temp = [guid]::NewGuid().guid
+	New-Item -ItemType File -Value $string.ToString() -Path $path -Name "$temp.eml" | Out-Null
+}
