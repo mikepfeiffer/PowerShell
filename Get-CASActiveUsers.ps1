829825c2af6e487aaf5e9e3a0ee517119273d5bf
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:05:19 -0700

    Get count of users connected to CAS via RPC/OWA

---
+++ Get-CASActiveUsers.ps1
@@ -0,0 +1,25 @@
+﻿function Get-CASActiveUsers {
+  [CmdletBinding()]
+    param(
+    [Parameter(Position=0, ParameterSetName="Value", Mandatory=$true)]
+    [String[]]$ComputerName,
+    [Parameter(Position=0, ParameterSetName="Pipeline", ValueFromPipelineByPropertyName=$true, Mandatory=$true)]
+    [String]$Name
+  )
+
+  process {
+    switch($PsCmdlet.ParameterSetName) {
+      "Value" {$servers = $ComputerName}
+      "Pipeline" {$servers = $Name}
+    }
+    $servers | %{
+      $RPC = Get-Counter "\MSExchange RpcClientAccess\User Count" -ComputerName $_
+      $OWA = Get-Counter "\MSExchange OWA\Current Unique Users" -ComputerName $_
+      New-Object PSObject -Property @{
+        Server = $_
+        "RPC Client Access" = $RPC.CounterSamples[0].CookedValue
+        "Outlook Web App" = $OWA.CounterSamples[0].CookedValue
+      }
+    }
+  }
+}
