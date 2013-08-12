6e894acd57bb901500561a957da4774a47519e2d
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:06:14 -0700

    Merge PDF Files w/ PDFsharp

---
+++ Merge-PDF.ps1
@@ -0,0 +1,17 @@
+﻿function Merge-PDF {            
+    Param($path, $filename)
+    
+    Add-Type -Path C:\assemblies\PdfSharp.dll                       
+            
+    $output = New-Object PdfSharp.Pdf.PdfDocument            
+    $PdfReader = [PdfSharp.Pdf.IO.PdfReader]            
+    $PdfDocumentOpenMode = [PdfSharp.Pdf.IO.PdfDocumentOpenMode]                        
+            
+    foreach($i in (gci $path *.pdf -Recurse)) {            
+        $input = New-Object PdfSharp.Pdf.PdfDocument            
+        $input = $PdfReader::Open($i.fullname, $PdfDocumentOpenMode::Import)            
+        $input.Pages | %{$output.AddPage($_)}            
+    }                        
+            
+    $output.Save($filename)            
+}
