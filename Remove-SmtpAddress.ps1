c522b69703ea184f15ea34779bc503fc4cbec7b1
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:06:47 -0700

    Remove all email address of specific domain

---
+++ Remove-SmtpAddress.ps1
@@ -0,0 +1,14 @@
+﻿function Remove-SmtpAddress{
+    param(
+        $Domain
+    )
+
+    process {
+        foreach($i in Get-Mailbox -ResultSize Unlimited) {
+　         $i.EmailAddresses |
+　　　         ?{$_.AddressString -like "*@$domain"} | %{
+　　　　　         Set-Mailbox $i -EmailAddresses @{remove=$_}
+　　　         }
+        }
+    }
+}
