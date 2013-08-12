a58cad1f609518d73dbf96275109fdb509ece79c
mikepfeiffer <mike@pfeiffer.ms>
2013-08-12 15:04:37 -0700

    Check/disable account lockout status

---
+++ AccountLockOut.ps1
@@ -0,0 +1,49 @@
+﻿function Get-AccountLockOut {
+	param(
+		[Parameter(Position=0, Mandatory=$true)]
+        [System.String]
+		$Identity,
+		[Parameter(Position=1, Mandatory=$false)]
+        [System.String]
+		$Domain = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().RootDomain.Name
+	)
+
+    if(([AppDomain]::CurrentDomain.GetAssemblies() | 
+        %{$_.ManifestModule.name}) -notcontains "System.DirectoryServices.AccountManagement.dll") {
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    }
+
+	$pc = [System.DirectoryServices.AccountManagement.PrincipalContext]
+	$up = [System.DirectoryServices.AccountManagement.UserPrincipal]
+
+	$context = New-Object $pc -ArgumentList "Domain", $domain
+	$user = $up::FindByIdentity($context,$Identity)
+
+	New-Object PSObject -Property @{
+		Username = $Identity
+		IsLockedOut = $user.IsAccountLockedOut()
+	}
+}
+
+function Disable-AccountLockOut {
+	param(
+		[Parameter(Position=0, Mandatory=$true)]
+        [System.String]
+		$Identity,
+		[Parameter(Position=1, Mandatory=$false)]
+        [System.String]
+		$Domain = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().RootDomain.Name
+	)
+
+    if(([AppDomain]::CurrentDomain.GetAssemblies() | 
+        %{$_.ManifestModule.name}) -notcontains "System.DirectoryServices.AccountManagement.dll") {
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    }
+
+	$pc = [System.DirectoryServices.AccountManagement.PrincipalContext]
+	$up = [System.DirectoryServices.AccountManagement.UserPrincipal]
+
+	$context = New-Object $pc -ArgumentList "Domain", $domain
+	$user = $up::FindByIdentity($context,$Identity)
+	$user.UnlockAccount()
+}
