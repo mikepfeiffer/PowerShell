b95aea56e2b7a99dd2cf295225f2c6b20ec7d2ec
Mike Pfeiffer <mike@pfeiffer.ms>
2014-02-17 05:08:50 -0800

    Create EC2 Security Group Rule

---
+++ New-EC2SecurityGroupRule.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $Id,
+    $CIDRBlocks,
+    $FromPort,
+    $ToPort,
+    $Protocol
+)
+
+$CIDR = @()
+$CIDR += $CIDRBlocks
+
+$ipPermissions = New-Object Amazon.EC2.Model.IpPermission -Property @{
+    IpProtocol = $Protocol
+    FromPort = $FromPort
+    ToPort = $ToPort
+    IPRanges = $CIDR
+}
+
+Grant-EC2SecurityGroupIngress -GroupId $Id -IpPermissions $ipPermissions
